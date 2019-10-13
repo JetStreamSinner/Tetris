eed60f71e43467ca7f795a68eb6d8c3cc14cfa7e
Jetstream <bigalex934@gmail.com>
2019-10-13 16:04:27 +0300

    Append drawing of playing field grid

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -2,41 +2,59 @@
 #include "MainWindow.h"
 
 
-MainWindow::MainWindow(const int min_width, const int min_height, QMainWindow * parent)
+MainWindow::MainWindow ( const int min_width, const int min_height, QMainWindow * parent )
 {
-    setFixedSize(min_width, min_height);
+    setFixedSize ( min_width, min_height );
 }
 
-void MainWindow::paintEvent(QPaintEvent * event)
+void MainWindow::paintEvent ( QPaintEvent * event )
 {
-    QPainter painter(this);
-    drawField(painter);
+    QPainter painter ( this );
+    drawField ( painter );
+    drawFieldGrid ( painter );
 }
 
-void MainWindow::drawInfo(QPainter& painter)
+void MainWindow::drawInfo ( QPainter& painter )
 {
     
 }
 
-
-void MainWindow::drawField(QPainter& painter)
+void MainWindow::drawFieldGrid ( QPainter& painter )
 {
-    // Side of the quad in field at px 
+    // Draw horizontal lines
+    for ( auto i = 0; i < row_count; i++ ) {
+        const QPoint left_point(delta_x, i * side_size + delta_y);
+        const QPoint right_point(delta_x + column_count * side_size, i * side_size + delta_y);
+        painter.drawLine(left_point, right_point);
+    }
+
+    // Draw vertical lines
+    for ( auto i = 0; i < column_count; i++ ) {
+        const QPoint up_point(delta_x + i * side_size, delta_y);
+        const QPoint down_point(delta_x + i * side_size, delta_y + side_size * row_count);
+        painter.drawLine(up_point, down_point);
+    }
+}
+
+
+void MainWindow::drawField ( QPainter& painter )
+{
+    // Side of the quad in field at px
     const auto side_size = 25;
-    
+
     const auto delta_x = 30;
     const auto delta_y = 30;
-    
-    QPoint up_left(0 + delta_x, 0 + delta_y);
-    QPoint up_right(0 + delta_x + side_size * row_count, 0 + delta_y);
-    QPoint down_left(0 + delta_x, height() - delta_y);
-    QPoint down_right(0 + delta_x + side_size * row_count, height() - delta_y);
-    
-    painter.drawLine(up_left,    up_right);
-    painter.drawLine(up_left,    down_left);
-    painter.drawLine(down_left,  down_right);
-    painter.drawLine(down_right, up_right);
-    
+
+    QPoint up_left ( 0 + delta_x, 0 + delta_y );
+    QPoint up_right ( 0 + delta_x + side_size * column_count, 0 + delta_y );
+    QPoint down_left ( 0 + delta_x, side_size * row_count + delta_y );
+    QPoint down_right ( 0 + delta_x + side_size * column_count, side_size * row_count + delta_y );
+
+    painter.drawLine ( up_left,    up_right );
+    painter.drawLine ( up_left,    down_left );
+    painter.drawLine ( down_left,  down_right );
+    painter.drawLine ( down_right, up_right );
+
 }
 
 
--- MainWindow.h
+++ MainWindow.h
@@ -6,20 +6,23 @@
 class MainWindow : public QMainWindow
 {
 private:
-    static const auto row_count    = 10;
-    static const auto column_count = 20;
+    static const auto row_count    = 20;
+    static const auto column_count = 10;
     static const auto side_size    = 25;
     static const auto delta_x      = 30;
     static const auto delta_y      = 30;
+
     
+    // Draw field bounds
+    void drawField ( QPainter& painter );
+    void drawFieldGrid ( QPainter& painter );
     
-    void drawField(QPainter& painter);
-    void drawInfo(QPainter&  painter);
-    
+    void drawInfo ( QPainter&  painter );
+
 public:
-    MainWindow(const int min_width, const int min_height, QMainWindow * parent = nullptr);
-    
-    void paintEvent(QPaintEvent * event);
+    MainWindow ( const int min_width, const int min_height, QMainWindow * parent = nullptr );
+
+    void paintEvent ( QPaintEvent * event );
 };
 
 
